76722f3942c449929bbe130db354391afdaf9a9b
Siedlar <ksiedlarski@gmail.com>
2020-04-04 15:45:50 +0200

    Zmiana pomyslu organizacji klas'

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,13 +1,20 @@
 public class User {
     private String imie;
     private String nazwisko;
+    private String login;
+    private String haslo;
 
-    public User() {
+    public User(String login, String haslo) {
+        this.login = login;
+        this.haslo = haslo;
     }
 
-    public User(String imie, String nazwisko) {
+    public User(String imie, String nazwisko, String login, String haslo) {
+
         this.imie = imie;
         this.nazwisko = nazwisko;
+        this.login = login;
+        this.haslo = haslo;
     }
 
     public String getImie() {
@@ -24,13 +31,5 @@ public class User {
 
     public void setNazwisko(String nazwisko) {
         this.nazwisko = nazwisko;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "imie='" + imie + '\'' +
-                ", nazwisko='" + nazwisko + '\'' +
-                '}';
     }
 }
